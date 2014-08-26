a71b05695f2264b0723ece9abec937a557b268a6
Hai Nguyen <hai5nguy@gmail.com>
2014-08-26 11:17:18 -0400

    url highlighting works, 3 hours

--- FullFrontalPaging.js
+++ FullFrontalPaging.js
@@ -15,69 +15,66 @@ if (Meteor.isClient) {
 */
 
 
-var imageUrlRegex = /https?:\/\/(?:[a-z\-]+\.)+[a-z]{2,6}(?:\/[^\/#?]+)+\.(?:jpe?g|gif|png)/i;
+var IMAGE_URL_REGEX = /https?:\/\/(?:[a-z\-]+\.)+[a-z]{2,6}(?:\/[^\/#?]+)+\.(?:jpe?g|gif|png)/i;
 
 Chats = new Meteor.Collection("chats");
 
+
+
+
+
 if (Meteor.isClient) {
-    Template.chats.chats = function () {
-        return Chats.find();
-    }
-
-    Template.image.url = function()
-    {
-        return "http://placehold.it/350x450"
-    }
-
-    Template.chatsubmit.events({
-        'click input#send': function(event) {
-            var form = $(event.currentTarget).closest("form");
-            var write = form.find('#write');
-            var message = write.val();
-
-            Chats.insert({ message: message });
-
-            
-            var imageUrl = findImageUrl(message);
-            if (imageUrl) {
-              $('#imagecontainer > img').attr('src', imageUrl);
-            }
-
-            
-            write.val('')
 
 
-            return false;
-        }
-    });
 
-    Handlebars.registerHelper("embedUrl", function(text) {
-      var matches = text.match(imageUrlRegex);
-      matches.forEach(function(match, i) {
-        text = surroundWithAnchor(text, url);
-      });
-      return text;
-    });
+  Template.chats.chats = function () {
+    return Chats.find();
+  }
 
-    function surroundWithAnchor(text, url) {
-      return text.replace(url, "<a href='" + url + "'>" + url + "</a>");
-    }
+  Template.image.url = function() {
+    return "http://placehold.it/350x450"
+  }
 
-    function findImageUrl(text) {
-      var matches = text.match(imageUrlRegex);
-      if (matches.length > 0) {
-        return matches[0];
-      } else {
-        return null;
-      }
-    }
+  Template.chatsubmit.events({
+    'click input#send': function(event) {
+      var form = $(event.currentTarget).closest("form");
+      var write = form.find('#write');
+      var message = write.val();
 
-    function embedImageUrlIntoText(text) {
-      var imageUrl = findImageUrl(text);
+      Chats.insert({ message: message });
+
+      var imageUrl = findImageUrl(message);
       if (imageUrl) {
-        return text.replace(imageUrl, "<a href='" + imageUrl + "'>" + imageUrl + "</a>");
-      } else {
-        return text;
+        $('#imagecontainer > img').attr('src', imageUrl);
       }
+
+      write.val('')
+
+      return false;
     }
+  });
+
+  UI.registerHelper("embedUrl", function(text) {
+    var matches = text.match(IMAGE_URL_REGEX);
+    if (!matches) return text;
+    matches.forEach(function(match, i) {
+      text = surroundWithAnchor(text, match);
+    });
+    return text;
+  });
+
+  function surroundWithAnchor(text, url) {
+    return text.replace(url, "<a href='" + url + "'>" + url + "</a>");
+  }
+
+  function findImageUrl(text) {
+    var matches = text.match(IMAGE_URL_REGEX);
+
+    if (matches) {
+      return matches[0];
+    } else {
+      return null;
+    }
+  }
+
 }
